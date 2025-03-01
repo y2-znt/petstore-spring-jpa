9c367791a70d1e0d842543fa3cd60a46b21b8183
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-01 21:28:56 +0100

    feat: add address initialization in PetstoreApplication

--- src/main/java/com/example/petstore/PetstoreApplication.java
+++ src/main/java/com/example/petstore/PetstoreApplication.java
@@ -1,11 +1,34 @@
 package com.example.petstore;
 
+import com.example.petstore.model.*;
+import com.example.petstore.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class PetstoreApplication implements CommandLineRunner {
+
+    private final AnimalRepository animalRepository;
+	private final AddressRepository addressRepository;
+	private CatRepository catRepository;
+	private FishRepository fishRepository;
+	private PetStoreRepository petStoreRepository;
+	private ProductRepository productRepository;
+
+	@Autowired
+	public PetstoreApplication(AddressRepository addressRepository, AnimalRepository animalRepository, CatRepository catRepository, FishRepository fishRepository, PetStoreRepository petStoreRepository, ProductRepository productRepository) {
+        this.animalRepository = animalRepository;
+		this.catRepository = catRepository;
+		this.fishRepository = fishRepository;
+		this.petStoreRepository = petStoreRepository;
+		this.productRepository = productRepository;
+		this.addressRepository = addressRepository;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetstoreApplication.class, args);
@@ -14,5 +37,13 @@ public class PetstoreApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
+
+		// Création des adresses
+		Address address1 = new Address("10", "rue de la paix", "Paris", "75000");
+		Address address2 = new Address("20", "rue de la liberté", "Lyon", "69000");
+		Address address3 = new Address("30", "rue de la fraternité", "Marseille", "13000");
+		addressRepository.save(address1);
+		addressRepository.save(address2);
+		addressRepository.save(address3);
 	}
 }
